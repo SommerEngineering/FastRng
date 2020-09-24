bc8d4ffe45bf7d5c633ae8f87be923deb364c134
Thorsten Sommer <mail@tsommer.org>
2020-09-24 20:20:56 +0200

    Defined test categories

---
+++ FastRngTests/TestCategories.cs
@@ -0,0 +1,11 @@
+namespace FastRngTests
+{
+    public class TestCategories
+    {
+        public const string COVER = "cover";
+        public const string PERFORMANCE = "performance";
+        public const string NORMAL = "normal";
+        public const string EXAMPLE = "example";
+        public const string LONG_RUNNING = "long running";
+    }
+}
