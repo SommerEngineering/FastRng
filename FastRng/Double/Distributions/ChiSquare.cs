9ba0eb9478fbe7995eda9b60d5643204efa6e96a
Thorsten Sommer <mail@tsommer.org>
2020-09-26 12:44:56 +0200

    Added check for parameter

--- FastRng/Double/Distributions/ChiSquare.cs
+++ FastRng/Double/Distributions/ChiSquare.cs
@@ -5,10 +5,22 @@ namespace FastRng.Double.Distributions
 {
     public sealed class ChiSquare : IDistribution
     {
+        private double degreesOfFreedom = 1.0;
+        
         public IRandom Random { get; set; }
 
-        public double DegreesOfFreedom { get; set; } = 1.0;
-        
+        public double DegreesOfFreedom
+        {
+            get => this.degreesOfFreedom;
+            set
+            {
+                if(value <= 0.0)
+                    throw new ArgumentOutOfRangeException(message: "DegreesOfFreedom must be greater than 0", null);
+                
+                this.degreesOfFreedom = value;
+            }
+        }
+
         public async ValueTask<double> GetDistributedValue(CancellationToken token)
         {
             if (this.Random == null)
