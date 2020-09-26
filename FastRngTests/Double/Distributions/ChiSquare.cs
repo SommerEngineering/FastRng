b810bede2a0db21a482cd6c0d2bf6af7a2112bcd
Thorsten Sommer <mail@tsommer.org>
2020-09-26 21:49:41 +0200

    Initial

---
+++ FastRngTests/Double/Distributions/ChiSquare.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using FastRng.Double;
+using NUnit.Framework;
+
+namespace FastRngTests.Double.Distributions
+{
+    [ExcludeFromCodeCoverage]
+    public class ChiSquare
+    {
+        [Test]
+        [Category(TestCategories.COVER)]
+        [Category(TestCategories.NORMAL)]
+        public async Task TestChiSquareDistribution01()
+        {
+            const double DOF = 7.0;
+            const double MEAN = DOF;
+            const double VARIANCE = 2 * DOF;
+            
+            var dist = new FastRng.Double.Distributions.ChiSquare{ DegreesOfFreedom = DOF };
+            var stats = new RunningStatistics();
+            var rng = new MultiThreadedRng();
+            
+            for (var n = 0; n < 100_000; n++)
+                stats.Push(await rng.NextNumber(dist));
+            
+            rng.StopProducer();
+            TestContext.WriteLine($"mean={MEAN} vs. {stats.Mean}");
+            TestContext.WriteLine($"variance={VARIANCE} vs {stats.Variance}");
+            
+            Assert.That(stats.Mean, Is.EqualTo(MEAN).Within(0.4), "Mean is out of range");
+            Assert.That(stats.Variance, Is.EqualTo(VARIANCE).Within(0.4), "Variance is out of range");
+        }
+        
+        [Test]
+        [Category(TestCategories.COVER)]
+        [Category(TestCategories.NORMAL)]
+        public async Task TestChiSquareGeneratorWithRange01()
+        {
+            var rng = new MultiThreadedRng();
+            var samples = new double[1_000];
+            for (var n = 0; n < samples.Length; n++)
+                samples[n] = await rng.NextNumber(-1.0, 1.0, new FastRng.Double.Distributions.ChiSquare());
+            
+            rng.StopProducer();
+            Assert.That(samples.Min(), Is.GreaterThanOrEqualTo(-1.0), "Min out of range");
+            Assert.That(samples.Max(), Is.LessThanOrEqualTo(1.0), "Max out of range");
+        }
+        
+        [Test]
+        [Category(TestCategories.COVER)]
+        [Category(TestCategories.NORMAL)]
+        public async Task TestChiSquareGeneratorWithRange02()
+        {
+            var rng = new MultiThreadedRng();
+            var samples = new double[1_000];
+            for (var n = 0; n < samples.Length; n++)
+                samples[n] = await rng.NextNumber(0.0, 1.0, new FastRng.Double.Distributions.ChiSquare());
+            
+            rng.StopProducer();
+            Assert.That(samples.Min(), Is.GreaterThanOrEqualTo(0.0), "Min is out of range");
+            Assert.That(samples.Max(), Is.LessThanOrEqualTo(1.0), "Max is out of range");
+        }
+
+        [Test]
+        [Category(TestCategories.COVER)]
+        [Category(TestCategories.NORMAL)]
+        public void ParameterTest01()
+        {
+            var dist = new FastRng.Double.Distributions.ChiSquare();
+            
+            Assert.Throws<ArgumentOutOfRangeException>(() => dist.DegreesOfFreedom = 0);
+            Assert.Throws<ArgumentOutOfRangeException>(() => dist.DegreesOfFreedom = -78);
+            Assert.DoesNotThrow(() => dist.DegreesOfFreedom = 0.0001);
+            Assert.DoesNotThrow(() => dist.DegreesOfFreedom = 4);
+        }
+
+        [Test]
+        [Category(TestCategories.COVER)]
+        [Category(TestCategories.NORMAL)]
+        public async Task NoRandomNumberGenerator01()
+        {
+            var dist = new FastRng.Double.Distributions.ChiSquare();
+            Assert.DoesNotThrowAsync(async () => await dist.GetDistributedValue());
+            Assert.That(await dist.GetDistributedValue(), Is.NaN);
+        }
+    }
+}
