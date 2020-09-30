94844a18e752b7e4c9695eac076efa275996450f
Thorsten Sommer <mail@tsommer.org>
2020-09-30 20:24:11 +0200

    Fixed tolerance

--- FastRngTests/Double/Distributions/Normal.cs
+++ FastRngTests/Double/Distributions/Normal.cs
@@ -32,8 +32,8 @@ namespace FastRngTests.Double.Distributions
             TestContext.WriteLine($"mean={MEAN} vs. {stats.Mean}");
             TestContext.WriteLine($"variance={STANDARD_DEVIATION * STANDARD_DEVIATION} vs {stats.Variance}");
             
-            Assert.That(stats.Mean, Is.EqualTo(MEAN).Within(0.4), "Mean is out of range");
-            Assert.That(stats.Variance, Is.EqualTo(STANDARD_DEVIATION*STANDARD_DEVIATION).Within(0.1), "Variance is out of range");
+            Assert.That(stats.Mean, Is.EqualTo(MEAN).Within(0.01), "Mean is out of range");
+            Assert.That(stats.Variance, Is.EqualTo(STANDARD_DEVIATION*STANDARD_DEVIATION).Within(0.01), "Variance is out of range");
         }
 
         [Test]
